e4e2c307bb9f9db4fcbd899f3bf4d81a02984a44
Maksym <90103151+MaksVibe@users.noreply.github.com>
2022-04-26 18:43:24 +0300

    Add methods

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
-require("dotenv").config();
+
 // process.env.SECRET_KEY;
 // process.env.NODE_ENV;
 
